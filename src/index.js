b27d74efd0448c8118743d000b340f8a2c53cb40
unknown <aquinasmoriasi@gmail.com>
2022-07-13 19:09:23 +0300

    generate html dynamically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
 import './style.css';
 
-const h1 = document.querySelector('h1');
-h1.classList.add('h1');
+const list = [
+  {
+    description: 'Go to the shop',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Play chess',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Pick up groceries',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Visit Laura',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Watch a movie',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Kill a mocking bird',
+    completed: false,
+    index: 0,
+  },
+];
+
+const taskList = document.getElementById('task-list');
+
+for (let j = 0; j < list.length; j += 1) {
+  const li = document.createElement('li');
+  li.setAttribute('draggable', true);
+
+  const input = document.createElement('input');
+  input.setAttribute('contenteditable', false);
+  input.setAttribute('type', 'checkbox');
+
+  const span = document.createElement('span');
+  span.setAttribute('class', 'input');
+  span.setAttribute('contenteditable', true);
+
+  const i = document.createElement('i');
+  i.setAttribute('class', 'bi bi-three-dots-vertical');
+
+  span.textContent += list[j].description;
+  li.append(input, span, i);
+  taskList.appendChild(li);
+  list[j].index += j;
+}
