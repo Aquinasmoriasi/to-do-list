95464730fa7f96b18c25b5f8411f180be83db5c2
unknown <aquinasmoriasi@gmail.com>
2022-07-14 15:12:21 +0300

    Add local storage

--- src/index.js
+++ src/index.js
@@ -1,82 +1,8 @@
 import './style.css';
+import './update.js';
+// import Sortable from '../node_modules/sortablejs/modular/sortable.complete.esm';
 
-const list = [
-  {
-    description: 'Go to the shop',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Play chess',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Pick up groceries',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Visit Laura',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Watch a movie',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Kill a mocking bird',
-    completed: false,
-    index: 0,
-  },
-];
-
-const taskList = document.getElementById('task-list');
-
-for (let j = 0; j < list.length; j += 1) {
-  const li = document.createElement('li');
-  li.setAttribute('draggable', true);
-
-  const input = document.createElement('input');
-  input.setAttribute('contenteditable', 'false');
-  input.setAttribute('type', 'checkbox');
-
-  const span = document.createElement('span');
-  span.setAttribute('class', 'input');
-  span.setAttribute('contenteditable', 'true');
-
-  const i = document.createElement('i');
-  i.setAttribute('class', 'bi bi-three-dots-vertical');
-
-  span.textContent += list[j].description;
-  li.append(input, span, i);
-  taskList.appendChild(li);
-  list[j].index += j;
-}
-
-const text = document.querySelectorAll('li span');
-text.forEach((t) => {
-  t.addEventListener('focus', () => {
-    const selection = window.getSelection();
-    const range = document.createRange();
-    selection.removeAllRanges();
-    range.selectNodeContents(t);
-    range.collapse(false);
-    selection.addRange(range);
-    t.focus();
-  });
-});
-
-const strike = () => {
-  const text = document.querySelectorAll('li span');
-  text.forEach((t) => {
-    t.style.textDecoration = 'line-through';
-  });
-};
-
-const check = document.querySelectorAll('li input');
-if (check.checked === true) {
-  strike();
-}
+// new Sortable(todosListEl, {
+//   handle: '.drag',
+//   animation: 150,
+// });
---
+++ src/update.js
@@ -0,0 +1,79 @@
+class Task {
+  constructor(description, completed = false, index = 0) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  static task() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+  }
+
+  static addTask(task) {
+    const taskItem = document.querySelector('textarea');
+    const tasks = this.task();
+    tasks.push(task);
+    taskItem.value = '';
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static remove(index) {
+    const tasks = this.task();
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static showTasks() {
+    const tasks = this.task();
+    const taskList = document.getElementById('task-list');
+    for (let j = 0; j < tasks.length; j += 1) {
+      const li = document.createElement('li');
+      li.setAttribute('draggable', 'true');
+
+      const input = document.createElement('input');
+      input.setAttribute('contenteditable', 'false');
+      input.setAttribute('type', 'checkbox');
+
+      const span = document.createElement('span');
+      span.setAttribute('class', 'input');
+      span.id = tasks[j].index;
+      span.setAttribute('contenteditable', 'true');
+
+      const i = document.createElement('i');
+      i.setAttribute('class', 'bi bi-three-dots-vertical');
+
+      span.innerHTML += tasks[j].description;
+      li.append(input, span, i);
+      taskList.appendChild(li);
+    }
+  }
+}
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  const taskItem = document.querySelector('textarea').value;
+  if (taskItem !== null) {
+    const task = new Task(taskItem);
+    Task.showTasks();
+    Task.addTask(task);
+  }
+  e.preventDefault();
+});
+
+const text = document.querySelectorAll('li span');
+text.forEach((t) => {
+  t.addEventListener('focus', () => {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    selection.removeAllRanges();
+    range.selectNodeContents(t);
+    range.collapse(false);
+    selection.addRange(range);
+    t.focus();
+  });
+});
