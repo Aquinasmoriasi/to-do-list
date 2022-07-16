28329986abb3134f7731d52608219dff9dd2106b
Aquinas Moriasi <104029494+Aquinasmoriasi@users.noreply.github.com>
2022-07-16 15:59:57 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import './style.css';
+import Task from './update.js';
+
+const showAllTasks = () => {
+  const tasks = Task.task();
+  tasks.forEach((task) => {
+    Task.showTasks(task);
+  });
+};
+document.addEventListener('DOMContentLoaded', showAllTasks());
+
+document.querySelector('.text-input').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const tasks = Task.task();
+  const taskItem = document.querySelector('textarea').value.trim();
+  const task = new Task(taskItem, Object.keys(tasks).length + 1);
+  Task.addTask(task);
+  Task.showTasks(task);
+  window.location.reload();
+});
+
+const text = document.querySelectorAll('li span');
+
+const menu = document.querySelectorAll('.bi-three-dots-vertical');
+
+text.forEach((t) => {
+  t.addEventListener('dblclick', () => {
+    t.setAttribute('readonly', 'readonly');
+  });
+  t.addEventListener('keyup', (e) => {
+    const index = e.target.value;
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      t.removeAttribute('readonly');
+      t.textContent = index;
+    }
+  });
+});
+
+menu.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    if (!item.classList.contains('bi-trash')) {
+      item.classList.add('bi-trash');
+    } else {
+      item.classList.remove('bi-trash');
+    }
+  });
+  item.addEventListener('mouseout', () => {
+    if (!item.classList.contains('bi-trash')) {
+      item.classList.add('bi-trash');
+    } else {
+      item.classList.remove('bi-trash');
+    }
+  });
+  item.addEventListener('click', () => {
+    const trash = document.querySelectorAll('.bi-trash');
+    trash.forEach((tr) => {
+      tr.addEventListener('click', (e) => {
+        let tasks = Task.task();
+        const { id } = e.target.parentNode;
+        const modifTasksBef = tasks.slice(0, id);
+        modifTasksBef.pop();
+        const modifTasksAft = tasks.slice(id);
+        modifTasksAft.forEach((t) => {
+          t.index -= 1;
+        });
+        tasks = [...modifTasksBef, ...modifTasksAft];
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        window.location.reload();
+      });
+    });
+  });
+});
+
+const editInput = document.querySelectorAll('.input');
+
+editInput.forEach((input) => {
+  input.addEventListener('change', (e) => {
+    const input = e.target.parentNode.childNodes[1].value.trim();
+    const { id } = e.target.parentNode;
+    const tasks = Task.task();
+    tasks[(id - 1)].description = input;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  });
+});
---
+++ src/update.js
@@ -0,0 +1,49 @@
+export default class Task {
+  constructor(description, index) {
+    this.description = description;
+    this.completed = false;
+    this.index = index;
+  }
+
+  static task() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+  }
+
+  static addTask(task) {
+    const taskItem = document.querySelector('textarea');
+    const tasks = this.task();
+    tasks.push(task);
+    taskItem.value = '';
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    window.location.reload();
+  }
+
+  static showTasks(task) {
+    const taskList = document.getElementById('task-list');
+
+    const div = document.createElement('div');
+    div.setAttribute('draggable', 'true');
+    div.setAttribute('class', 'listed-task');
+    div.setAttribute('id', `${task.index}`);
+
+    const check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.setAttribute('class', 'check');
+
+    const input = document.createElement('input');
+    input.setAttribute('class', 'input');
+
+    const i = document.createElement('i');
+    i.setAttribute('class', 'bi bi-three-dots-vertical');
+
+    input.value = `${task.description}`;
+    div.append(check, input, i);
+    taskList.appendChild(div);
+  }
+}
